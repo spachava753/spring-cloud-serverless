56e62bca649646e97b158538eaaecfec1f8b9ba6
Shashank Pachava <spachava753@gmail.com>
2019-03-16 20:06:47 -0400

    Initial commit

---
+++ src/main/java/net/spblog/func/SpringCloudServerlessApplication.java
@@ -0,0 +1,13 @@
+package net.spblog.func;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudServerlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerlessApplication.class, args);
+	}
+
+}
